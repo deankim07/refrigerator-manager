f9506b346bb36b75d829e5672b72f6ae49c30044
dean-kim <keunu07@gmail.com>
2018-05-28 22:07:16 +0900

    add test factories about place

--- refrigerator_manager/apps/place/models.py
+++ refrigerator_manager/apps/place/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 from core.constant import PlaceConstant
 
@@ -8,3 +9,4 @@ class Place(models.Model):
     DB table for place save item
     """
     name = models.CharField(max_length=20, choices=PlaceConstant.TYPE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.PROTECT)
--- refrigerator_manager/apps/place/tests/test_factories.py
+++ refrigerator_manager/apps/place/tests/test_factories.py
@@ -3,22 +3,22 @@ from django.test import TestCase
 
 from users.tests.factories import UserFactory
 
-from items.models import Paprika
-from items.tests.factories import PaprikaFakeFactory
+from place.models import Place
+from place.tests.factories import PlaceFakeFactory
 
 User = get_user_model()
 
 
-class PaprikaFakeFactoryTest(TestCase):
+class PlaceFakeFactoryTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
         cls.user = UserFactory()
-        cls.paprika = PaprikaFakeFactory.create(user=cls.user)
+        cls.paprika = PlaceFakeFactory.create(user=cls.user)
 
     def test_user_created(self):
         self.assertIn('user-', self.user.username)
 
     def test_paprika_can_create(self):
-        self.assertEqual(Paprika.objects.count(), 1)
-        self.assertEqual(Paprika.objects.first().user_id, self.user.id)
+        self.assertEqual(Place.objects.count(), 1)
+        self.assertEqual(Place.objects.first().user_id, self.user.id)
