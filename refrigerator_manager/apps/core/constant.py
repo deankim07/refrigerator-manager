f20e5bf491f847cbe804619c65f4c731c968525f
dean-kim <keunu07@gmail.com>
2018-06-20 09:52:14 +0900

    fix constant

--- refrigerator_manager/apps/core/constant.py
+++ refrigerator_manager/apps/core/constant.py
@@ -8,35 +8,6 @@ class PlaceConstant:
     )
 
 
-# class CategoryConstant:
-#     VEGETABLE = 'VEGETABLE'
-#     SAUCE = 'SAUCE'
-#     BEEF = 'BEEF'
-#     PORK = 'PORK'
-#     NUT = 'NUT'
-#     MUSHROOM = 'MUSHROOM'
-#     FISH = 'FISH'
-#     DAIRY_PRODUCT = 'DAIRY_PRODUCT'
-#     GRAIN = 'GRAIN'
-#     INSTANT= 'INSTANT'
-#     SIDE_DISH = 'SIDE_DISH'
-#     FRUIT = 'FRUIT'
-#
-#     TYPE = (
-#         (VEGETABLE, 'Vegetable'),
-#         (SAUCE, 'Sauce'),
-#         (BEEF, 'Beef'),
-#         (PORK, 'Pork'),
-#         (NUT, 'Nut'),
-#         (MUSHROOM, 'Mushroom'),
-#         (FISH, 'Fish'),
-#         (DAIRY_PRODUCT, 'Dairy product'),
-#         (GRAIN, 'Grain'),
-#         (INSTANT, 'Instant'),
-#         (SIDE_DISH, 'Side dish'),
-#         (FRUIT, 'Fruit'),
-#     )
-
 class VegetableConstant:
     PAPRIKA = 'PAPRIKA'
     GREEN_PUMPKIN = 'GREEN PUMPKIN'
