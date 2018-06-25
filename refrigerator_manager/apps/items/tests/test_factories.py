d663613bdb14f841e2037a55ff4f643469776449
dean-kim <keunu07@gmail.com>
2018-06-25 10:44:08 +0900

    test coverage

--- refrigerator_manager/apps/items/tests/test_factories.py
+++ refrigerator_manager/apps/items/tests/test_factories.py
@@ -3,8 +3,8 @@ from django.test import TestCase
 
 from users.tests.factories import UserFactory
 
-from items.models import Paprika
-from items.tests.factories import PaprikaFakeFactory
+from items.models import Vegetables
+from items.tests.factories import VegetablesFakeFactory
 
 User = get_user_model()
 
@@ -14,12 +14,12 @@ class PaprikaFakeFactoryTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.user = UserFactory()
-        cls.paprika = PaprikaFakeFactory.create(user=cls.user)
+        cls.paprika = VegetablesFakeFactory.create(user=cls.user)
 
     def test_user_created(self):
         self.assertIn('user-', self.user.username)
 
     def test_paprika_can_create(self):
-        self.assertEqual(Paprika.objects.count(), 1)
-        print(Paprika.objects.first().left_storage_period)
-        self.assertEqual(Paprika.objects.first().user_id, self.user.id)
+        self.assertEqual(Vegetables.objects.count(), 1)
+        print(Vegetables.objects.first().left_storage_period)
+        self.assertEqual(Vegetables.objects.first().user_id, self.user.id)
